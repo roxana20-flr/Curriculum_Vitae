852ef54ccea4e61d854e08b261eab58325387f87
alexbran8 <alexbran8@gmail.com>
2022-10-11 21:32:50 +0300

    react router fix

--- src/components/main_page/Page.js
+++ src/components/main_page/Page.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Page.css";
 import SocialFlow from "./SocialFlow.js";
 import Navbar from "../sidebar/Navbar";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Reports from '../pages/Reporst';
 import Products from '../pages/Products';
@@ -14,9 +14,9 @@ function Page() {
             <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
+          <Route path='/' element={<Home />} />
+          <Route path='/reports' element={<Reports />} />
+          {/* <Route path='/products' component={Products} /> */}
         </Routes>
       </Router>
             <h1 className="name">Savu Roxana Florina</h1>
